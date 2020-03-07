be8c74a65d5f3ba39a98a38f0c0ff780034e59df
Neil Grogan <neil@grogan.ie>
2020-03-07 17:17:00 +0000

    New PDF OCR script

---
+++ pdf_ocr.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""
+pdf_ocr.py by Neil Grogan, 2020
+
+A script to run convert PDFs from scanner to PDF/A with embedded text via OCR
+"""
+import sys
+from datetime import datetime
+from pathlib import Path
+import time
+import logging
+from watchdog.observers import Observer
+from watchdog.events import PatternMatchingEventHandler
+import ocrmypdf
+
+
+class PDFHandler(PatternMatchingEventHandler):
+    patterns = ["*.PDF", "*.pdf"]
+
+    def process(self, event):
+        """
+        event.event_type 
+            'modified' | 'created' | 'moved' | 'deleted'
+        event.is_directory
+            True | False
+        event.src_path
+            path/to/observed/file
+        """
+        # the file will be processed there
+        if "Scan" in event.src_path:
+            now = datetime.today().strftime('%y-%m-%d')
+            new_path = Path(event.src_path).parent.joinpath(now +' output.pdf').resolve()
+            ocrmypdf.ocr(event.src_path, new_path, force_ocr=True,)
+        print(event.src_path, event.event_type)
+
+    def on_modified(self, event):
+        self.process(event)
+
+    def on_created(self, event):
+        self.process(event)
+
+
+if __name__ == "__main__":
+    path = sys.argv[1] if len(sys.argv) > 1 else '.'
+    event_handler = PDFHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
